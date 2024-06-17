0883e3f4135f17fd2ce115fab7b32ca79d7a9a09
Christophe-Calmes <christophe.calmes@laplateforme.io>
2024-06-17 17:38:57 +0200

    first step

---
+++ sources/univers/objets/sqlUnivers.php
@@ -0,0 +1,9 @@
+<?php
+class sqlUnivers 
+{
+    protected $nt;
+
+    public function __construct () {
+        $this->nt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    }
+}
---
+++ sources/univers/objets/templateUnivers.php
@@ -0,0 +1,9 @@
+<?php
+require ('templateUnivers.php');
+
+class templateUnivers extends sqlUnivers 
+{
+    public function __construct () {
+        parent::__construct();
+    }
+}
