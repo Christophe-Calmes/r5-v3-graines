e8148a6fe1e916ffc5d58736b7885d2009a2a3ef
Christophe-Calmes <christophe.calmes@laplateforme.io>
2024-09-04 06:58:27 +0200

    Algorithm miniature

--- sources/miniatures/cud/Creat/addMiniature.php
+++ sources/miniatures/cud/Creat/addMiniature.php
@@ -1,12 +1,28 @@
 <?php
 // encodeRoutage(96)
 require ('../sources/miniatures/objets/sqlMiniatures.php');
+function intervalMove ($data) {
+    if(($data > 18)||($data <0)) {
+        return false;
+    }
+    return true;
+}
 $miniatureTraitement = new sqlMiniatures ();
-print_r($_POST);
-$arrayKeys = ['dqm','dc', 'healtPoint', 'armor', 'typeTroop', 'miniatureSize','fligt','stationnaryFligt'];
+$arrayKeys = ['nameMiniature', 'move', 'dqm','dc', 'healtPoint', 'armor', 'typeTroop', 'miniatureSize','fligt','stationnaryFligt'];
 $controle_POST = array();
+
 if(checkPostFields ($arrayKeys, $_POST)) {
-    
+    array_push($controle_POST, sizePost(filter($_POST[$arrayKeys[0]]), 60));
+    array_push($controle_POST, $checkId->controleIntegerPK(filter($_POST[$arrayKeys[1]])));
+    array_push($controle_POST, intervalMove(filter($_POST[$arrayKeys[1]])));
+    array_push($controle_POST, $miniatureTraitement->checkDice (filter($_POST[$arrayKeys[2]])));
+    array_push($controle_POST, $miniatureTraitement->checkDice (filter($_POST[$arrayKeys[3]])));
+    array_push($controle_POST, $miniatureTraitement->checkHealPoint (filter($_POST[$arrayKeys[4]])));
+    array_push($controle_POST, $miniatureTraitement->checkArmor (filter($_POST[$arrayKeys[5]])));
+    array_push($controle_POST, $miniatureTraitement->checkTypeTroop (filter($_POST[$arrayKeys[6]])));
+    array_push($controle_POST, $miniatureTraitement->checkMiniatureSize (filter($_POST[$arrayKeys[7]])));
+    array_push($controle_POST, $miniatureTraitement->checkYes (filter($_POST[$arrayKeys[8]])));
+    array_push($controle_POST, $miniatureTraitement->checkYes (filter($_POST[$arrayKeys[9]])));
 }
 echo '<br/>';
-print_r($controle_POST);
+$miniatureTraitement->solveMiniaturePrice($_POST);
--- sources/miniatures/objets/sqlMiniatures.php
+++ sources/miniatures/objets/sqlMiniatures.php
@@ -8,38 +8,139 @@ class sqlMiniatures
     protected $miniatureSize;
     protected $yes;
     public function __construct () {
-        $this->dice = [['id'=>'0', 'valueDice'=> 6, 'nameDice'=> 'D6'],
-        ['id'=>'1', 'valueDice'=> 8, 'nameDice'=> 'D8'],
-        ['id'=>'2', 'valueDice'=> 10, 'nameDice'=> 'D10'],
-        ['id'=>'3', 'valueDice'=> 12, 'nameDice'=> 'D12']];
-        $this->armour = [['id'=>0, 'valueArmour' => 0.5, 'nameArmour'=> '-'],
-        ['id'=>1, 'valueArmour' => 1, 'nameArmour'=> '9+'],
-        ['id'=>2, 'valueArmour' => 1.30, 'nameArmour'=> '8+'],
-        ['id'=>3, 'valueArmour' => 1.40, 'nameArmour'=> '7+'],
-        ['id'=>4, 'valueArmour' => 1.50, 'nameArmour'=> '6+'],
-        ['id'=>5, 'valueArmour' => 1.60, 'nameArmour'=> '5+'],
-        ['id'=>6, 'valueArmour' => 1.70, 'nameArmour'=> '4+'],
-        ['id'=>7, 'valueArmour' => 1.80, 'nameArmour'=> '3+'],];
-        $this->healtPoint = [['id'=>0, 'valueHealtPoint'=>1, 'healtPoint'=> 1],
-        ['id'=>1, 'valueHealtPoint'=>2, 'healtPoint'=> 2],
-        ['id'=>2, 'valueHealtPoint'=> 4, 'healtPoint'=> 3],
-        ['id'=>3, 'valueHealtPoint'=> 8, 'healtPoint'=> 4],
-        ['id'=>4, 'valueHealtPoint'=> 16, 'healtPoint'=> 5],
-        ['id'=>5, 'valueHealtPoint'=> 32, 'healtPoint'=> 6],];
-        $this->typesTroupe = [['id'=>0, 'valueTypeTroupe'=>0.5, 'nameTroupe'=>'Civilian', 'commandePoint'=>0.25],
-        ['id'=>1, 'valueTypeTroupe'=>1, 'nameTroupe'=>'Conscript soldier', 'commandePoint'=>0.5],
-        ['id'=>2, 'valueTypeTroupe'=>2, 'nameTroupe'=>'Regular soldier', 'commandePoint'=>0.75],
-        ['id'=>3, 'valueTypeTroupe'=>4, 'nameTroupe'=>'Elite soldier', 'commandePoint'=>1],
-        ['id'=>4, 'valueTypeTroupe'=>4.25, 'nameTroupe'=>'Veteran soldier', 'commandePoint'=>1.25],
-        ['id'=>5, 'valueTypeTroupe'=>6, 'nameTroupe'=>'Officer', 'commandePoint'=>2],
-        ['id'=>6, 'valueTypeTroupe'=>10, 'nameTroupe'=>'Executive officer', 'commandePoint'=>4],];
-        $this->miniatureSize = [['id'=>0, 'valueSize'=> 2, 'NameSize'=>'Small miniatures'],
-        ['id'=>1, 'valueSize'=> 2.5, 'NameSize'=>'Standard'],
-        ['id'=>2, 'valueSize'=> 4, 'NameSize'=>'Large miniature'],
-        ['id'=>3, 'valueSize'=> 3, 'NameSize'=>'Giant']];
-        $this->yes = [['id'=>0, 'name'=> 'No'], ['id'=>1, 'name'=>'Yes']];
+        $this->dice = [['id'=>1, 'valueDice'=> 6, 'nameDice'=> 'D6'],
+        ['id'=>2, 'valueDice'=> 8, 'nameDice'=> 'D8'],
+        ['id'=>3, 'valueDice'=> 10, 'nameDice'=> 'D10'],
+        ['id'=>4, 'valueDice'=> 12, 'nameDice'=> 'D12']];
+        $this->armour = [['id'=>1, 'valueArmour' => 0.5, 'nameArmour'=> '-'],
+        ['id'=>2, 'valueArmour' => 1, 'nameArmour'=> '9+'],
+        ['id'=>3, 'valueArmour' => 1.30, 'nameArmour'=> '8+'],
+        ['id'=>4, 'valueArmour' => 1.40, 'nameArmour'=> '7+'],
+        ['id'=>5, 'valueArmour' => 1.50, 'nameArmour'=> '6+'],
+        ['id'=>6, 'valueArmour' => 1.60, 'nameArmour'=> '5+'],
+        ['id'=>7, 'valueArmour' => 1.70, 'nameArmour'=> '4+'],
+        ['id'=>8, 'valueArmour' => 1.80, 'nameArmour'=> '3+'],];
+        $this->healtPoint = [['id'=>1, 'valueHealtPoint'=>1, 'healtPoint'=> 1],
+        ['id'=>2, 'valueHealtPoint'=>2, 'healtPoint'=> 2],
+        ['id'=>3, 'valueHealtPoint'=> 4, 'healtPoint'=> 3],
+        ['id'=>4, 'valueHealtPoint'=> 8, 'healtPoint'=> 4],
+        ['id'=>5, 'valueHealtPoint'=> 16, 'healtPoint'=> 5],
+        ['id'=>6, 'valueHealtPoint'=> 32, 'healtPoint'=> 6],];
+        $this->typesTroupe = [['id'=>1, 'valueTypeTroupe'=>1, 'nameTroupe'=>'Civilian', 'commandePoint'=>0.10],
+        ['id'=>2, 'valueTypeTroupe'=>4, 'nameTroupe'=>'Conscript soldier', 'commandePoint'=>0.4],
+        ['id'=>3, 'valueTypeTroupe'=>6, 'nameTroupe'=>'Regular soldier', 'commandePoint'=>0.6],
+        ['id'=>4, 'valueTypeTroupe'=>12, 'nameTroupe'=>'Elite soldier', 'commandePoint'=>1.2],
+        ['id'=>5, 'valueTypeTroupe'=>14, 'nameTroupe'=>'Veteran soldier', 'commandePoint'=>1.4],
+        ['id'=>6, 'valueTypeTroupe'=>10, 'nameTroupe'=>'Officer', 'commandePoint'=>2],
+        ['id'=>7, 'valueTypeTroupe'=>12, 'nameTroupe'=>'Executive officer', 'commandePoint'=>2.4],];
+        $this->miniatureSize = [['id'=>1, 'valueSize'=> 2, 'NameSize'=>'Small miniatures'],
+        ['id'=>2, 'valueSize'=> 2.5, 'NameSize'=>'Standard'],
+        ['id'=>3, 'valueSize'=> 4, 'NameSize'=>'Large miniature'],
+        ['id'=>4, 'valueSize'=> 3, 'NameSize'=>'Giant']];
+        $this->yes = [['id'=>1, 'name'=> 'No'], ['id'=>2, 'name'=>'Yes']];
     }
-    public function solveMiniaturePrice () {
-        
+
+    public function checkYes ($id) {
+        $index = array_search($id, array_column($this->dice, 'id'));
+        if($index !== false) {
+            return true;
+        } 
+        return false;
+    }
+    public function checkDice ($id) {
+        $index = array_search($id, array_column($this->dice, 'id'));
+        if($index !== false) {
+            return true;
+        } 
+        return false;
+    }
+    public function checkHealPoint ($id) {
+        $index = array_search($id, array_column($this->healtPoint, 'id'));
+        if($index !== false) {
+            return true;
+        } 
+        return false;
+    }
+    public function checkArmor ($id) {
+        $index = array_search($id, array_column($this->armour, 'id'));
+        if($index !== false) {
+            return true;
+        } 
+        return false;
+    }
+    public function checkTypeTroop ($id) {
+        $index = array_search($id, array_column($this->typesTroupe, 'id'));
+        if($index !== false) {
+            return true;
+        } 
+        return false;
+    }
+    public function checkMiniatureSize ($id) {
+        $index = array_search($id, array_column($this->miniatureSize, 'id'));
+        if($index !== false) {
+            return true;
+        } 
+        return false;
+    }
+    private function getDiceValue ($id) {
+        $index = array_search($id, array_column($this->dice, 'id'));
+        return $this->dice[$index]['valueDice'];
+    }
+    private function getHealtPointValue ($id) {
+        $index = array_search($id, array_column($this->healtPoint, 'id'));
+        return $this->healtPoint[$index]['valueHealtPoint'];
+    }
+    private function getArmour ($id) {
+        $index = array_search($id, array_column($this->armour, 'id'));
+        return $this->armour[$index]['valueArmour'];
+    }
+    private function getTypesTroupe ($id) {
+        $index = array_search($id, array_column($this->typesTroupe, 'id'));
+        return $this->typesTroupe[$index]['valueTypeTroupe'];
+    }
+    private function getMiniatureSize ($id) {
+        $index = array_search($id, array_column($this->miniatureSize, 'id'));
+        return $this->miniatureSize[$index]['valueSize'];
+    }
+    private function boolYes ($id) {
+        if($id == 1) {
+            return 1;
+        }
+        return 3;
+    }
+    public function solveMiniaturePrice ($data) {
+    $result = 0;
+    /* Solve move */
+    $valueMove = log($data['move']);
+    $fligth = $this->boolYes ($data['fligt']);
+    $stationnaryFligth = $this->boolYes ($data['stationnaryFligt']);
+    $valueMove = $valueMove * $fligth * $stationnaryFligth;
+    /* Solve move */
+    $valueDQM = $this->getDiceValue ($data['dqm']);
+    $valueDC = $this->getDiceValue ($data['dc']);
+    $valueHealtPoint = $this->getHealtPointValue ($data['healtPoint']);
+    $valueTypeTroupe = $this->getTypesTroupe ($data['typeTroop']);
+    $valueMiniatureSize = $this->getMiniatureSize ($data['typeTroop']);
+    $coefArmor = $this->getArmour ($data['armor']);
+
+    print_r($data);
+    echo '<br/>Move';
+    print_r($valueMove);
+    echo '<br/>DQM';
+    print_r($valueDQM);
+    echo '<br/>DC';
+    print_r($valueDC);
+    echo '<br/>HealtPoint';
+    print_r($valueHealtPoint);
+    echo '<br/>Armor';
+    print_r($coefArmor);
+    echo '<br/>typeTroop';
+    print_r($valueTypeTroupe);
+    echo '<br/>miniatureSize';
+    print_r($valueMiniatureSize);
+    $result = ($valueMove + $valueDQM + $valueDC + $valueHealtPoint + $valueTypeTroupe + $valueMiniatureSize) * $coefArmor;
+    echo '<br/>miniaturePrice';
+    print_r($result);
+     return $result;   
     }
 }
