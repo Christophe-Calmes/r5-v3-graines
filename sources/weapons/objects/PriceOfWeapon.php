6a12ce0a76d6dcead973788b09d67e4048266bf9
Christophe-Calmes <christophe.calmes@laplateforme.io>
2025-03-25 15:54:25 +0100

    modification weapon price

--- sources/weapons/objects/PriceOfWeapon.php
+++ sources/weapons/objects/PriceOfWeapon.php
@@ -5,52 +5,85 @@ class PriceOfWeapon
     public function __construct () {
         $this->power = [1.10, 2.15, 3.3, 4.6, 6.2, 7.4];
     }
-    private function baseWeaponPrice ($arrayWeapon) {
-        /* Base */
-        $price =  $this->power[$arrayWeapon[0]];
-        /* overPower */
-        if($arrayWeapon[1] == 1) {
+    private function creatParamWeapon ($arrayWeapon) {
+        switch (count($arrayWeapon)) {
+            case 10:
+                $paramWeapon = [
+                    'power'=>$arrayWeapon[0], 
+                    'overPower'=>$arrayWeapon[1],
+                    'heavy'=>$arrayWeapon[2], 
+                    'spell'=>$arrayWeapon[3], 
+                    'assault'=>$arrayWeapon[4], 
+                    'saturation'=>$arrayWeapon[5], 
+                    'rateOfFire'=>$arrayWeapon[6], 
+                    'rangeWeapon'=>$arrayWeapon[7], 
+                    'templateType'=>$arrayWeapon[8], 
+                    'blastDice'=>$arrayWeapon[9]];
+                return $paramWeapon;
+                break;
+            case 8:
+                $paramWeapon = [
+                    'power'=>$arrayWeapon[0], 
+                    'overPower'=>$arrayWeapon[1],
+                    'heavy'=>$arrayWeapon[2], 
+                    'spell'=>$arrayWeapon[3], 
+                    'assault'=>$arrayWeapon[4], 
+                    'saturation'=>$arrayWeapon[5], 
+                    'rateOfFire'=>$arrayWeapon[6], 
+                    'rangeWeapon'=>$arrayWeapon[7]];
+                return $paramWeapon;
+                break;
+            default:
+            $paramWeapon = [
+                'power'=>$arrayWeapon[0], 
+                'overPower'=>$arrayWeapon[1],
+                'heavy'=>$arrayWeapon[2], 
+                'spell'=>$arrayWeapon[3]];
+            return $paramWeapon;
+            break;
+        }
+     
+    }
+    private function baseWeaponPrice ($paramWeapon) {
+        $price =  $this->power[$paramWeapon['power']];
+        if($paramWeapon['overPower'] == 1) {
             $price = $price + 2.15;
         }
-        /* Spell */
-        if($arrayWeapon[3] == 1) {
+        if($paramWeapon['spell'] == 1) {
             $price = $price * 1.2;
         }
-        /* Heavy weapon */
-        if($arrayWeapon[2] == 1) {
+        if($paramWeapon['heavy']  == 1) {
             $price = $price * 1.5;
         }
         return $price;
     }
     public function closeWeaponPrice ($arrayWeapon) {
-        return $this->baseWeaponPrice ($arrayWeapon);
+        $paramWeapon = $this->creatParamWeapon ($arrayWeapon);
+        return $this->baseWeaponPrice ($paramWeapon);
     }
     public function shootingWeaponPrice ($arrayWeapon) {
-        $price = $this->baseWeaponPrice ($arrayWeapon);
-        /* Assault*/
-        if($arrayWeapon[4] == 1) {
+        $paramWeapon = $this->creatParamWeapon ($arrayWeapon);
+        $price = $this->baseWeaponPrice ($paramWeapon );
+        if($paramWeapon['assault']  == 1) {
             $price = $price + 0.1;
         }
-        /* Saturation*/
-        if($arrayWeapon[5] == 1) {
+        if($paramWeapon['saturation']== 1) {
             $price = $price + 1.2;
         }
-        /* Rate of fire */
-        //$price = $price + ($arrayWeapon[6]/12); 
-        $price = $price + ($arrayWeapon[6]/3); 
-        /* range weapon */
+        $price = $price + ($paramWeapon['rateOfFire']/3); 
+     
         if($arrayWeapon[7] != 0) {
-            $price += log($arrayWeapon[7]);
+            $price += log($paramWeapon['rangeWeapon']);
         }
         return round($price, 3);
     }
     public function blastWeaponPrice ($arrayWeapon) {
-        $price = $this->shootingWeaponPrice ($arrayWeapon);
+        $paramWeapon = $this->creatParamWeapon ($arrayWeapon);
+        $price = $this->shootingWeaponPrice ($paramWeapon);
         $blastSurface = [0.78, 3.14, 7.07, 10.89, 12.57, 19.63];
         ['1"(BA)', '2"(BA)', '3"(BA / GW)', 'Blast(GW)', '4"(BA)', '5"(GW)'];
         $blastDicePrice = [0.2, 0.4, 0.8, 1.6, 2.5];
-        /* templateType & blastDice */
-        $price = $price + ($blastSurface[$arrayWeapon[8]] * $blastDicePrice[$arrayWeapon[9]]);
+        $price = $price + ($blastSurface[$paramWeapon['templateType']] * $blastDicePrice[$paramWeapon['blastDice']]);
         return $price;
     }
     public function specialRulesPrice ($idWeapon, $rawPrice) {
