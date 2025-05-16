8d389a745ab140bd19f064007acaf1af756c1db2
Christophe-Calmes <christophe.calmes@laplateforme.io>
2025-05-16 15:16:02 +0200

    corrective name picture

--- functions/functionToken.php
+++ functions/functionToken.php
@@ -27,3 +27,17 @@ function IntToken ($size) {
     }
     return $token;
 }
+function genRandomName ($size) {
+    $alpha = 'abcdefghijklmnopqrstuvwxyz';
+    for ($i=0; $i < 6 ; $i++) {
+        $alpha = str_shuffle($alpha).$alpha;
+    }
+    $token = NULL;
+    for ($i=0 ; $i < $size  ; $i++ ) {
+      $number = rand(0, strlen($alpha));
+      $letter = substr($alpha, $number, 1);
+      $token = $token.$letter;
+      //$token =  $token.substr($alpha, rand(0,strlen($alpha)));
+    }
+    return $token;
+}
